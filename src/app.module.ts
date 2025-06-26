6e673082f222254c231d3193ba220ebc5f15e41d
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-06-26 23:32:59 +0530

    SetUp the .env and config files for configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config/config';
 
 // const uri =
 //   'mongodb+srv://lahirul:farc35wlD1JM124R@cluster0.frhslxf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
@@ -11,9 +14,26 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://lahirul:farc35wlD1JM124R@cluster0.frhslxf.mongodb.net/Tasks?retryWrites=true&w=majority&appName=Cluster0',
-    ),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [config],
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      global: true,
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt.secret'),
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('databaseUrl.connectionString'),
+      }),
+      inject: [ConfigService],
+    }),
     TasksModule,
     AuthModule,
   ],
