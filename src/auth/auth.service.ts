1e95429aef1f2ba8993d1fb6d150cff919fcea41
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-06-26 21:41:13 +0530

    Created login Logic And need to implement JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { SignupDto } from './dtos/signup.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
@@ -30,9 +34,18 @@ export class AuthService {
   }
 
   async login(credentials: { email: string; password: string }) {
+    const { email, password } = credentials;
     // Find if user exists by email
-
-    // Compare enetered password with existing hashed password
-
+    const user = await this.UserModel.findOne({ email });
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+    // Compare entered password with existing hashed password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
     //Generate JWT tokens
+    return { message: 'Login successful', userId: user._id };
+  }
 }
